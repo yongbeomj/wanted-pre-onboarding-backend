d611eddbf1c868ba35f5165503abc7445035c1c1
Yongbeom Jang <alex.ybjang@gmail.com>
2023-10-17 17:29:05 +0900

    refactor: 채용공고 등록 메소드명 수정 및 불필요 코드 제거

--- src/main/java/com/wanted/preonboarding/controller/JobOpeningController.java
+++ src/main/java/com/wanted/preonboarding/controller/JobOpeningController.java
@@ -2,9 +2,7 @@ package com.wanted.preonboarding.controller;
 
 import com.wanted.preonboarding.dto.JobOpeningDto;
 import com.wanted.preonboarding.service.JobOpeningService;
-import com.wanted.preonboarding.util.ResponseUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,14 +14,8 @@ public class JobOpeningController {
 
     // 채용공고 등록
     @PostMapping
-    public Object test(@RequestBody JobOpeningDto jobOpeningDto) {
-        try {
-            jobOpeningService.savePost(jobOpeningDto);
-
-            return ResponseUtil.success(jobOpeningDto);
-        } catch (Exception e) {
-            return ResponseUtil.error(e, HttpStatus.BAD_REQUEST);
-        }
+    public Object savePost(@RequestBody JobOpeningDto jobOpeningDto) {
+        return jobOpeningService.savePost(jobOpeningDto);
     }
 
     // 채용공고 수정
